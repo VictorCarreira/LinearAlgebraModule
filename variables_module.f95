2a608cba6ab6a4a16ae8f40e35967dbc63efa5af
VictorCarreira <carreiravr@gmail.com>
2017-05-02 00:15:57 -0300

    Adding a header with some variables

---
+++ variables_module.f95
@@ -0,0 +1,11 @@
+MODULE variables
+  !!!!!!!!!!!!!!!!!!!Variáveis!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      IMPLICIT NONE
+
+CONTAINS
+
+      REAL(KIND=8), ALLOCATABLE, DIMENSION(:,:)::A
+      REAL(KIND=8), ALLOCATABLE, DIMENSION(:,:)::B
+      REAL(KIND=8), ALLOCATABLE, DIMENSION(:,:)::E
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+END MODULE variables
